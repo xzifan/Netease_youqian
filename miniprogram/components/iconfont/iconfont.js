3bf0e4865b2a85383d5b8abd4fa7069139b438fa
XCifer <13607960126@139.com>
2018-11-06 01:09:31 +0800

    icon-iconfont

---
+++ miniprogram/components/iconfont/iconfont.js
@@ -0,0 +1,16 @@
+Component({
+  properties: {
+    type: {
+      type: String,
+      value: ''
+    },
+    color: {
+      type: String,
+      value: '#000000'
+    },
+    size: {
+      type: Number,
+      value: '45'
+    }
+  }
+})
