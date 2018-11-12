3c4a400c5f3b614110999a3949167809cb2aa7d6
XCifer <13607960126@139.com>
2018-11-12 18:48:59 +0800

    在appã.js上加载数据

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,15 +1,22 @@
-//app.js
-App({
-  onLaunch: function () {
-    
-    if (!wx.cloud) {
-      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
-    } else {
-      wx.cloud.init({
-        traceUser: true,
-      })
-    }
 
-    this.globalData = {}
-  }
+App({
+  globalData: {
+    appid: 'wx37bd8c039572adcb	',//appid需自己提供，此处的appid我随机编写
+    secret: "46377c2e6036460307f16c1c03a8a739",
+    data:null
+  },
+  onLaunch: function () {
+    wx.request({
+      url: 'https://www.easy-mock.com/mock/5be37ffe1a0b241d97ce4425/youqian/accounts#!method=get',
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: (res) => {
+        console.log(res.data)
+        Object.assign(this.globalData, res.data)//将原有值替换更新
+        wx.setStorageSync("data", res.data)
+        console.log(this.globalData)
+      }
+    })
+  },
 })
